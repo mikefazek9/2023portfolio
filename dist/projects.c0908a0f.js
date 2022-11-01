1d659a8de411ff9a0702ede10f3323be0796c1a4
Michael Fazekas <71743477+mikefazek9@users.noreply.github.com>
2022-11-01 16:23:00 -0500

    Add files via upload

---
+++ dist/projects.c0908a0f.js
@@ -0,0 +1,2 @@
+var icon=document.getElementById("icon"),icon1=document.getElementById("a"),icon2=document.getElementById("b"),icon3=document.getElementById("c"),nav=document.getElementById("menu"),blue=document.getElementById("green-container");icon.addEventListener("click",(function(){icon1.classList.toggle("a"),icon2.classList.toggle("c"),icon3.classList.toggle("b"),nav.classList.toggle("show"),blue.classList.toggle("slide")}));
+//# sourceMappingURL=projects.c0908a0f.js.map
